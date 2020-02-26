3fab87dcc911d751f5c8e374488b82429078c325
Max <maxime.quero@gmail.com>
2020-02-26 18:34:58 +0100

    * Adding mangoose dependency * Configuring connectin to mongodb database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/moovi', { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
