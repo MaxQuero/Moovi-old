2c942265c36024b4ec583a2700cb8250705a987e
Max <maxime.quero@gmail.com>
2022-09-08 19:25:57 +0200

    Migration de Create-React-App vers Vite

--- frontend/src/app.constants.ts
+++ frontend/src/app.constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
   public static API_DEFAULT='https://api.themoviedb.org/3';
   public static API_KEY='729494a29c30941aac184290ea7c53a1';
-  public static FRONT_URL = 'http://www.moovi.fr:3000';
+  public static FRONT_URL = 'http://www.moovi.fr:5173';
   public static BACK_URL = 'http://www.moovi.fr:5000';
 }
